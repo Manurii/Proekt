279055bda34575b49479fe8dc1752a8296b5411b
Manurii <anzelikah9677@gmail.com>
2025-04-25 09:19:38 +0300

    test GUI,new

--- interfa.py
+++ interfa.py
@@ -1,13 +1,21 @@
 from tkinter import*
+from tkinter import ttk
 
 win = Tk()
 win.title("My PROJECT")
 win.geometry('500x300')
 win.resizable(False,False) #заборона зміни вікна
 #Елементи керування
-#Напис
+#Запис
 nap = Label(win, text="Текст для напису", font=('Comic Sans MS', 20), bg="green", fg="white")
-nap.place(relx=0.1, rely=0.4)
-
-
+nap.place(relx=0.2, rely=0.1, anchor="center")
+#Поле для введення
+ent = Entry(win, font=('Comic Sans MS', 20), width=4)
+ent.place(relx=0.1, rely=0.2)
+#Кнопка
+but = Button(win, text='Click', font=('Comic Sans MS', 20), width=7, height=3)
+but.place(relx=0.1, rely=0.35)
+#Випадаючий список
+comb = ttk.Combobox(win, font=('Comic Sans MS', 15), width=10, values=['option1','option2','option3','option4','option5'] )
+comb.place(relx=0.4, rely=0.35)
 win.mainloop()
